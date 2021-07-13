8a2954c5bc3fa3b1a25b1c1bf13d001d2ca0ba73
EILEEN CATERIN <katerinmoreno.g@gmail.com>
2021-07-12 19:24:20 -0500

    modificado

--- first.py
+++ first.py
@@ -1,10 +1,29 @@
+from googleapiclient.http import MediaFileUpload
 from Google import Create_Service
 
-CLIENT_SECRET_FILE = 'secret.json'
+
+CLIENT_SECRET_FILE = 'client_secret.json.json'
 API_NAME= 'drive'
 API_VERSION = 'v3'
 SCOPES= ['https://www.googleapis.com/auth/drive']
 
-service = Create_Service (CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-print(dir(service))
+folder_id='1ihMtJ54CW8jUuEWhihNN5uHBWer-9I8Z'
+file_names = ['Datos_drive.xlsx']
+mime_types = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+
+for file_name, mime_type in zip(file_names, mime_types):
+    file_metadata = {
+         'name': file_name,
+         'parents': [folder_id]
+    }
+
+    media = MediaFileUpload('./conexion/{0}'.format(file_name), mimetype = mime_type)
+
+    service.files().create(
+        body = file_metadata,
+        media_body = media,
+        fields = "id"
+
+    ).execute()
